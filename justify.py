7a2f922ac40af373b008f40ba8498f9361f728aa
Janus Friis Nielsen <jfn@fagidiot.dk>
2017-09-27 22:16:02 +0200

    Add comments

--- justify.py
+++ justify.py
@@ -1,8 +1,13 @@
 
+
 text = 'There are two different ways for rsync to contact a remote system: using a  remote-shell program as the transport (such as ssh or rsh) or contacting an rsync daemon directly via TCP.  The remote-shell transport is used whenever the source or destination path contains a single colon (:) separator after a  host  specification.   Contacting  an rsync daemon directly happens when the source or destination path contains a double colon (::) separator after a host specification, OR when an  rsync://  URL  is specified  (see  also  the "USING RSYNC-DAEMON FEATURES VIA A REMOTE-SHELL CONNECTION" section for an exception to this latter rule).'
 text = ' '.join(text.split())
 
-def left_align(text, width):
+
+def break_text(text, width):
+    '''
+        Break test according to width
+    '''
     lines = []
     words = text.split(' ')
     inx = 0
@@ -18,13 +23,16 @@ def left_align(text, width):
 
 
 print('x' * 40)
-left_aligned = left_align(text, 40)
-# print(left_aligned)
-for line in left_aligned:
+for line in break_text(text, 40):
     print(' '.join(line))
 
 
 def length_of_line(line):
+    '''
+        Compute length of line including spaces.
+        But (obviously) without a space after last
+        word
+    '''
     acc = 0
     for word in line[:-1]:
         acc += len(word) + 1
@@ -33,6 +41,11 @@ def length_of_line(line):
 
 
 def justify(lines, width):
+    '''
+        Justify text on lines with aligned left and right edges
+        Uses a list of extra spaces that must be interspersed
+        between the words
+    '''
     for line in lines:
         extra_spaces = width - length_of_line(line)
         spaces = list(' ' * extra_spaces)
@@ -45,8 +58,9 @@ def justify(lines, width):
             inx += 1
     return lines
 
+
 print('')
 print('x' * 40)
-justified = justify(left_aligned, 40)
+justified = justify(break_text(text, 40), 40)
 for line in justified:
     print(' '.join(line))
